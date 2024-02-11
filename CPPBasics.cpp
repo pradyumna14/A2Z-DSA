96d3865f52f30622c07449daa1afc0f984e77d4c
[pradyumna14] <pradyumnakattel14@gmail.com>
2024-02-12 00:29:24 +0530

    DOne with the Baiscs of C++

---
+++ CPPBasics.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> //this <bits/stdc++.h> includes all libraries at once!
+using namespace std;
+int main (){
+
+int i;
+for (i=1; i<=10; i++){
+cout << "Hello" << endl;
+}
+
+int j = 1;
+while(j<=10){
+    cout<<"hello bitch"<<endl;
+    j=j+1;
+}
+
+int k = 1;
+do {
+cout << "Jai Shree Ram" << endl;
+k++;
+} while (k<=10);
+
+ return 0;
+}
